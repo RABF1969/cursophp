86880b2d66f2576170f6d33d1f6103925a0c2922
RABF1969 <rabf44@gmail.com>
2023-04-23 20:32:43 -0300

    first commit

---
+++ ex003/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+   <?php 
+//         0x = hexadecimal 0b = binário 0 = octal
+//    $num = 300;
+//    echo "o valor da variavel e $num";
+
+// $v = "Antonio";
+// var_dump($v);
+// $num =3e2; // 3x10(2)
+// echo "o valor é $num";
+   ?> 
+</body>
+</html>
